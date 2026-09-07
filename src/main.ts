aecdbef551044c6b3b3ee92c1fb35cddb049327a
AbdenigoCoelho <abdeniotel@gmail.com>
2026-09-07 18:33:53 -0400

    feat: configura bootstrap da aplicacao com tratamento de erro

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(8080);
+}
+bootstrap().catch((err) => {
+  console.error('Erro ao iniciar a aplicação', err);
+  process.exit(1);
+});
