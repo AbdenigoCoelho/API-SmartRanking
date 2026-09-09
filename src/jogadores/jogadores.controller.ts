6332cdb46193af7930c6f7602720807f04c9a602
AbdenigoCoelho <abdeniotel@gmail.com>
2026-09-09 06:53:25 -0400

    fix: conecta controller ao service para persistir jogadores

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { JogadoresService } from './jogadores.service';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
+  constructor(private readonly jogadoresService: JogadoresService) {}
 
-   @Post()
-async criarAtualizarJogador(
-    @Body() criaJogadorDto: CriarJogadorDto) {
-    const { email } = criaJogadorDto;
-    return { email }; 
- }
-} 
+  @Post()
+  async criarAtualizarJogador(
+    @Body() criaJogadorDto: CriarJogadorDto,
+  ): Promise<void> {
+    await this.jogadoresService.criarAtualizarJogador(criaJogadorDto);
+  }
+}
