0e56edf7047627c17a8beee6e59ea0aaf341610f
AbdenigoCoelho <abdeniotel@gmail.com>
2026-09-09 06:52:54 -0400

    feat: substitui armazenamento em memoria por persistencia no MongoDB

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,56 +1,44 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
-import { Jogador } from './interfaces/jogador.interface';
+import { Jogador, JogadorDocument } from './interfaces/jogador.schema';
 
 @Injectable()
 export class JogadoresService {
-  private jogadores: Jogador[] = [];
   private readonly logger = new Logger(JogadoresService.name);
+
+  constructor(
+    @InjectModel(Jogador.name)
+    private readonly jogadorModel: Model<JogadorDocument>,
+  ) {}
 
   async criarAtualizarJogador(criaJogadorDto: CriarJogadorDto): Promise<void> {
     this.logger.log(`criaJogadorDto: ${JSON.stringify(criaJogadorDto)}`);
 
-    const jogadorEncontrado = this.jogadores.find(
-      (jogador) => jogador.email === criaJogadorDto.email,
-    );
+    const jogadorEncontrado = await this.jogadorModel
+      .findOne({ email: criaJogadorDto.email })
+      .exec();
 
     if (jogadorEncontrado) {
-      await this.atualizar(jogadorEncontrado, criaJogadorDto);
+      await this.atualizar(criaJogadorDto);
     } else {
       await this.criar(criaJogadorDto);
     }
   }
 
-  private criar(criaJogadorDto: CriarJogadorDto): Promise<void> {
-    return new Promise((resolve) => {
-      const jogador: Jogador = {
-        _id: Math.random().toString(36).substring(2, 15), // trocar por geração real de id (ex: uuid, ou deixado a cargo do banco)
-        ranking: 0,
-        posicao: 0,
-        urljogador: '',
-        ...criaJogadorDto,
-      };
-      this.logger.log(`jogador: ${JSON.stringify(jogador)}`);
-      this.jogadores.push(jogador);
-      resolve();
-    });
+  private async criar(criaJogadorDto: CriarJogadorDto): Promise<void> {
+    const jogadorCriado = new this.jogadorModel(criaJogadorDto);
+    this.logger.log(`jogador criado: ${JSON.stringify(jogadorCriado)}`);
+    await jogadorCriado.save();
   }
 
-  private atualizar(
-    jogadorEncontrado: Jogador,
-    criaJogadorDto: CriarJogadorDto,
-  ): Promise<void> {
-    return new Promise((resolve) => {
-      const index = this.jogadores.findIndex(
-        (jogador) => jogador._id === jogadorEncontrado._id,
-      );
-      if (index !== -1) {
-        this.jogadores[index] = {
-          ...this.jogadores[index],
-          ...criaJogadorDto,
-        };
-      }
-      resolve();
-    });
+  private async atualizar(criaJogadorDto: CriarJogadorDto): Promise<void> {
+    await this.jogadorModel
+      .findOneAndUpdate(
+        { email: criaJogadorDto.email },
+        { $set: criaJogadorDto },
+      )
+      .exec();
   }
 }
